0d4a1283eba65556ce4764fb928dcc3800d331f3
Cameron Reikes <cameronreikes@gmail.com>
2019-10-18 23:39:34 -0700

    Always reset godot settings file

--- bundle.sh
+++ bundle.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 if [ ! -f "tmp.tres" ]; then # avoid overriding previously cached tmp ( after failed script )
 	cp /home/creikey/.config/godot/editor_settings-3.tres tmp.tres
 fi
@@ -10,6 +8,8 @@ mkdir -p exports/web
 rm -r exports/web
 mkdir exports/web
 godot-headless --path src --export web "$(readlink -f ./exports/web/index.html)"
-butler push exports/web lajollagamesclub/pixel-floater:web 
+if [ "$?" == "0" ]; then
+	butler push exports/web lajollagamesclub/pixel-floater:web 
+fi
 
 mv tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
